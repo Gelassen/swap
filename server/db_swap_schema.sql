7c23281fe63f5ca6d978fe81790b1ac13172b00d
dkazakov <dmitrii.kazakov@gmail.com>
2022-05-11 17:31:01 +0300

    Remove redunded files

--- server/db_swap_schema.sql
+++ server/db_swap_schema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `db_swap` DEFAULT CHARACTER SET utf8mb3;/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `db_swap` DEFAULT CHARACTER SET utf8mb3;
 USE `db_swap`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
 --
@@ -64,3 +64,55 @@ CREATE TABLE `Service` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2022-04-29 12:56:46
+
+CREATE DATABASE  IF NOT EXISTS `test_db_swap` DEFAULT CHARACTER SET utf8mb3;
+USE `test_db_swap`;
+
+DROP TABLE IF EXISTS `Profile`;
+CREATE TABLE `Profile` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `contact` varchar(45) NOT NULL,
+  `secret` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`,`contact`,`secret`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `Service`;
+CREATE TABLE `Service` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(320) NOT NULL,
+  `date` bigint NOT NULL,
+  `offer` tinyint NOT NULL,
+  `index` varchar(450) DEFAULT NULL,
+  `profileId` int NOT NULL,
+  PRIMARY KEY (`id`,`profileId`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_id` (`profileId`),
+  CONSTRAINT `fk_id` FOREIGN KEY (`profileId`) REFERENCES `Profile` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+
+CREATE DATABASE  IF NOT EXISTS `prod_db_swap` DEFAULT CHARACTER SET utf8mb3;
+USE `prod_db_swap`;
+
+DROP TABLE IF EXISTS `Profile`;
+CREATE TABLE `Profile` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `contact` varchar(45) NOT NULL,
+  `secret` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`,`contact`,`secret`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `Service`;
+CREATE TABLE `Service` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(320) NOT NULL,
+  `date` bigint NOT NULL,
+  `offer` tinyint NOT NULL,
+  `index` varchar(450) DEFAULT NULL,
+  `profileId` int NOT NULL,
+  PRIMARY KEY (`id`,`profileId`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_id` (`profileId`),
+  CONSTRAINT `fk_id` FOREIGN KEY (`profileId`) REFERENCES `Profile` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
