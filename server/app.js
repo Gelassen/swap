154c26165d22d47401b8937749d66b6642abf0a9
Gelassen <dmitrii.kazakov@gmail.com>
2022-07-22 08:33:03 +0300

    Add router

--- server/app.js
+++ server/app.js
@@ -1,88 +1,18 @@
 const express = require('express');
 const app = express();
 const config = require('config');
-
-const pool = require('./database');
-const profile = require('./controllers/profile');
-const offers = require('./controllers/offers'); 
-const demands = require('./controllers/demands');
-const contacts = require('./controllers/contacts');
+const indexRouter = require('./routes/index');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(function(req, res, next) {
     console.log(`[REQUEST] ${JSON.stringify(req.path)} with query ${JSON.stringify(req.query)} at ${new Date().toISOString()}`);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Content-Type", "application/json; charset=utf-8");
     res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
-})
-
-/* account */
-app.get('/api/v1', function(req, res, next) {
-    res.send({msg: "Hello to open exchange platform!" })
-})
-
-app.post('/api/v1/account', function(req, res) {
-    pool.status();
-    profile.create(req, res);
-})
-
-app.get('/api/v1/account', function(req, res) {
-    pool.status();
-    profile.get(req, res);
-})
-
-app.delete('/api/v1/account/:id', function(req, res) {
-    pool.status();
-    profile.delete(req, res);
-})
-
-app.get('/api/v1/account/offers', function(req, res) {
-    pool.status();
-    res.send(405, { "payload": "Only POST and DELETE requests are allowed for this resource."})
-})
-
-app.post('/api/v1/account/offers', function(req, res) {
-    pool.status();
-    profile.addOffer(req, res);
-})
-
-app.post('/api/v1/account/demands', function(req, res) {
-    pool.status();
-    profile.addDemand(req, res);
-})
-
-app.delete('/api/v1/account/offers/:id', function(req, res) {
-    pool.status();
-    profile.deleteOffer(req, res);
-})
-
-app.delete('/api/v1/account/demands/:id', function(req, res) {
-    pool.status();
-    profile.deleteDemand(req, res);
-})
-
-/* offers part */
-app.get('/api/v1/offers', function(req, res) {
-    pool.status();
-    offers.get(req, res);
-})
-
-/* demands part */
-app.get('/api/v1/demands', function(req, res) {
-    pool.status();
-    demands.get(req, res);
-})
-
-/* chains part */
-
-/* contacts part */
-app.get('/api/v1/contacts', function(req, res) {
-    pool.status();
-    contacts.get(req, res);
-})
+});
+app.use('/', indexRouter);
 
 app.listen(config.get('config').webPort);
 
---
+++ server/routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const profile = require('../controllers/profile');
+const offers = require('../controllers/offers'); 
+const demands = require('../controllers/demands');
+const contacts = require('../controllers/contacts');
+
+router.get('/api/v1', function(req, res) {
+    res.send({msg: "Hello to open exchange platform!" })
+});
+router.post('/api/v1/account', profile.create);
+router.get('/api/v1/account', profile.get);
+router.delete('/api/v1/account/:id', profile.delete);
+router.get('/api/v1/account/offers', function(req, res) {
+    console.log('we are in the /api/v1/account/offers handler!')
+    res.send(405, { "payload": "Only POST and DELETE requests are allowed for this resource."})
+});
+router.post('/api/v1/account/offers', profile.addOffer);
+router.post('/api/v1/account/demands', profile.addDemand);
+router.delete('/api/v1/account/offers/:id', profile.deleteOffer);
+router.delete('/api/v1/account/demands/:id', profile.deleteDemand);
+
+router.get('/api/v1/offers', offers.get);
+
+router.get('/api/v1/demands', demands.get);
+
+router.get('/api/v1/contacts', contacts.get);
+
+module.exports = router;
