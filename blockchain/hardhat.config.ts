e2dce93a7c2d30f443f25118a733b7757051443c
Dmitry Kazakov <dmitrii.kazakov@gmail.com>
2022-11-11 16:27:08 +0300

    Update hardhat config file

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,12 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+const configChain = require('config');
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
-    }
+    },
+    goerli: {
+      url: configChain.get("goerly_testnet").goerlyApi,
+      accounts: [`0x${configChain.get("wallet").privateKey}`],
+      chainId: 5,
+    },
   }
   
 };
--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -1,23 +1,33 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const factoryMatchDynamicArray = await ethers.getContractFactory("MatchDynamicArray");
+  // const contractMatchDynamicArray = await factoryMatchDynamicArray.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  // const factoryUtils = await ethers.getContractFactory("Utils");
+  // const contractUtils = await factoryUtils.deploy();
+  // contractUtils.deployed()
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const factoryValue = await ethers.getContractFactory("SwapValue");
+  const contractValue = await factoryValue.deploy();
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  // const factory = await ethers.getContractFactory("SwapChain");
+  // const contract = await factory.deploy(
+  //     contractUtils.address, 
+  //     contractValue.address,
+  //     contractMatchDynamicArray.address
+  // );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
