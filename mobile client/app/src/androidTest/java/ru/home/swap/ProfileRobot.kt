443433944c2838aacc05896c17af99dbce589bc0
Dmitry Kazakov <dmitrii.kazakov@gmail.com>
2023-02-13 14:38:07 +0300

    Add sign-in main flow test coverage

--- mobile client/app/src/androidTest/java/ru/home/swap/ProfileRobot.kt
+++ mobile client/app/src/androidTest/java/ru/home/swap/ProfileRobot.kt
@@ -1,8 +1,11 @@
 package ru.home.swap
 
+import android.view.KeyEvent
+import android.widget.EditText
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -38,6 +41,31 @@ class ProfileRobot {
         return this
     }
 
+    // main flow
+
+    fun seesNavView(): ProfileRobot {
+        onView(withId(R.id.navigation_bar))
+            .check(matches(ViewMatchers.isDisplayed()))
+        return this
+    }
+
+    fun seesProfileTitle(input: String): ProfileRobot {
+        onView(withId(R.id.profile_title))
+            .check(matches(withText(input)))
+        onView(withId(R.id.offers_title))
+            .check(matches(withText(R.string.title_offers)))
+        onView(withId(R.id.demands_title))
+            .check(matches(withText(R.string.demands_title)))
+        return this
+    }
+
+    fun seesErrorDialog(input: String): ProfileRobot {
+        onView(withId(android.R.id.message)/*withText(R.string.default_error_title_dialog)*/)
+            .inRoot(isDialog())
+            .check(matches(withText(input)))
+        return this
+    }
+
     // Actions
 
     fun clickDebugButton() {
@@ -48,6 +76,22 @@ class ProfileRobot {
     fun clickSubmitButton() {
         onView(withId(R.id.confirm))
             .perform(ViewActions.click())
+    }
+
+    fun enterCustomDebugData(name: String, phone: String, secret: String, wallet: String) {
+        onView(withId(R.id.name))
+            .perform(ViewActions.replaceText(name))
+            .perform(ViewActions.pressKey(KeyEvent.KEYCODE_ENTER))
+        /*.perform(typeText(query), pressKey(KeyEvent.KEYCODE_ENTER)) // Unicode chars is not supported here */
+        onView(withId(R.id.contact_phone))
+            .perform(ViewActions.replaceText(phone))
+            .perform(ViewActions.pressKey(KeyEvent.KEYCODE_ENTER))
+        onView(withId(R.id.secret))
+            .perform(ViewActions.replaceText(secret))
+            .perform(ViewActions.pressKey(KeyEvent.KEYCODE_ENTER))
+        onView(withId(R.id.wallet_address))
+            .perform(ViewActions.replaceText(wallet))
+            .perform(ViewActions.pressKey(KeyEvent.KEYCODE_ENTER))
     }
 
 }
--- mobile client/app/src/androidTest/java/ru/home/swap/SignInFragmentTest.kt
+++ mobile client/app/src/androidTest/java/ru/home/swap/SignInFragmentTest.kt
@@ -9,6 +9,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import ru.home.swap.core.tests.NetworkIdlingResource
+import java.time.LocalDateTime
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -22,6 +23,7 @@ class SignInFragmentTest {
     fun setUp() {
         IdlingRegistry.getInstance().register(NetworkIdlingResource.countingIdlingResource)
         IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+        /*val context = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext*/
     }
 
     @After
@@ -31,7 +33,7 @@ class SignInFragmentTest {
     }
 
     @Test
-    fun onStart_enterDebugDataWIthRegisteredWalletAndSubmit_seesErrorDialog() {
+    fun onStart_whenServerAreTurnedOffEnterDebugDataAndSubmit_seesErrorDialog() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
@@ -43,6 +45,54 @@ class SignInFragmentTest {
             .seesSecret("onemoretime")
             .seesWalletAddress("0x62F8DC8a5c80db6e8FCc042f0cC54a298F8F2FFd")
             .clickSubmitButton()
+
+        robot.seesErrorDialog("Something went wrong")
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun onStart_enterDebugDataWithRegisteredWalletAndSubmit_seesErrorDialog() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        robot.clickDebugButton()
+
+        robot
+            .seesName("Dmitry")
+            .seesContact("+79207008090")
+            .seesSecret("onemoretime")
+            .seesWalletAddress("0x62F8DC8a5c80db6e8FCc042f0cC54a298F8F2FFd")
+            .clickSubmitButton()
+
+        robot.seesErrorDialog("Failed register the profile on chain with status exception")
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun onStart_enterDebugDataWithNotRegisteredAccountAndSubmit_seesEmptyProfileScreen() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+        // TODO I haven't found yet a way to automatically generate this input data, so it requires to do it manually
+        val newName = "Dmitry ${LocalDateTime.now()}"
+        val newPhone = "+7920700803"
+        val secret = "onemoretime"
+        val newWallet = "0x62F8DC8a5c80db6e8FCc042f0cC54a298F8F2006"
+
+        robot.enterCustomDebugData(newName, newPhone, secret, newWallet)
+        robot
+            .seesName(newName)
+            .seesContact(newPhone)
+            .seesSecret(secret)
+            .seesWalletAddress(newWallet)
+            .clickSubmitButton()
+
+        robot
+            .seesNavView()
+            .seesProfileTitle("\n\n\n${newName}")
+
+        activityScenario.close()
     }
 
 }
